62b8f6c3c0d13e480b11805e9df3294dc33b8976
Arkaprabha Ghosh <arkag70@gmail.com>
2022-12-01 18:01:24 +0530

    updated taxable sal calc

--- sal.py
+++ sal.py
@@ -1,26 +1,28 @@
+medical80D = 12000
+
 def salary(company):
 
 	print(f"\nFor {company}:")
 
 	if company == "APTIV":
 		pretaxnet = 1650995
-		taxable1 = 1386595
 		basic = 65967
 		foodcost = 3000
 		bonus = 0
 
 	elif company == "MERCEDES":
 		pretaxnet = 1443074
-		taxable1 = 1178674
 		basic = 47532
 		foodcost = 0
 		bonus = 142596
+	
+	taxable1 = pretaxnet - 252400 - medical80D + bonus
 
 	rent = [i* 1000 for i in range(9, 20)]
 
 	hra = [12*min(0.4*basic, (r - 0.1*basic)) for r in rent]
 
-	taxable2 = [(taxable1  - h) for h in hra]
+	taxable2 = [(taxable1 - h) for h in hra]
 
 	finaltax = [1.04*(112500 + 0.3*(t2 - 1000000)) if t2>1000000 else (12500 + 0.2*(t2 - 500000)) for t2 in taxable2]
 
